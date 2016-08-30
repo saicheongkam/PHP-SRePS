851ed004b97940deb23e62e271221a01cc1e17fa
xxfast <isuru@xfastgames.com>
2016-08-30 17:34:55 +1000

    modified the app.js

--- SRePS/js/app.js
+++ SRePS/js/app.js
@@ -15,7 +15,10 @@ app.config(
 
 app.controller('salesViewController', 
 	function($scope, $filter, Database){
-		$scope.sales = Database.getSales();
+		var dataPromise = Database.getSales();
+		dataPromise.$promise.then(function(result){
+			$scope.sales = result;
+		})
 		
 });
 
@@ -31,15 +34,29 @@ app.controller('detailedViewController',
 
 app.controller('addSaleViewController', 
 	function($scope, $filter, Database){
-		$scope.date = new Date();
-		$scope.cart = [{"batch_id":"1","product":"Doxycycline","qty":"2", "unitprice":"12.50"}];
+		$scope.calculateTotal = function(){
+			var total = 0;
+			$scope.cart.forEach(function(item){
+				total += parseFloat(item.unitprice) * parseInt(item.qty);
+			});
+			$scope.total = total;
+			return total;
+		}
 		$scope.addItem = function(toAdd){
 			$scope.cart.push({"batch_id":toAdd.batch,"product":toAdd.product,"qty":toAdd.qty,"unitprice":toAdd.unit_price});
+			$scope.calculateTotal();
+			document.getElementById('product_name').focus();
+			$('#pricetag').addClass('animated flipInX');
 		};
 		$scope.removeItem = function(toRemove){
 			var indexToRemove = $scope.cart.indexOf(toRemove);
 			if (indexToRemove>=0) $scope.cart.splice(indexToRemove,1);
+			$scope.calculateTotal();
 		}
+		$scope.date = new Date();
+		$scope.cart = [{"batch_id":"1","product":"Doxycycline","qty":"2", "unitprice":"12.50"}];
+		$scope.calculateTotal();
+	
 });
 
 // Data factory
@@ -55,7 +72,7 @@ app.factory("Data",
 
 
 // Services
-app.service('Database', function() {
+app.service('Database', function($http) {
 	var sales = [{
 				"id":100302, 
 				"date": new Date(),
@@ -69,9 +86,19 @@ app.service('Database', function() {
 					{"batch_id":"4","product":"Zoloft","qty":"1","unitprice":"12.50"}],
 				"staff":"James Bardock"
 			}];
+	
 	this.getSales = function () {
-		return sales;
+		var url = "api/salesapi.php/sales";
+		$http.get(url)
+		.then (function(response) {
+					alert('test');
+					return response.data;
+				}
+				,function(response) {
+					return {error:response.status,desc:response.statusText}
+		});
 	};
+	
 	this.getSale = function (sale_id) {
 		var tosend;
 		sales.forEach(function(sale){
@@ -92,4 +119,19 @@ app.run(
 				});
 				$(document).foundation();
 		});
+});
+
+//Directives
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
 });
